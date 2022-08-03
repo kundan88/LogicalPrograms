c8948ad01a702446cdc551c89d761263fa9cb074
Kundan <kundan kamble>
2022-08-03 10:27:33 -0800

    Added Fibonacci program

---
+++ FibonacciSeries/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LogicalProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FibonacciSeries.Fibonacci();
+        }
+    }
+}
